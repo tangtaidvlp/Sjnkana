1cebaa33dac6a173cffed3cceb503ee2c5166e32
TaiTang <tangtaidvlp@gmail.com>
2021-12-27 00:09:29 +0700

    Initial commit

---
+++ src/main/java/com/tommy/spring/sjnkana/SjnkanaApplication.java
@@ -0,0 +1,13 @@
+package com.tommy.spring.sjnkana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SjnkanaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SjnkanaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tommy/spring/sjnkana/SjnkanaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tommy.spring.sjnkana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SjnkanaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
